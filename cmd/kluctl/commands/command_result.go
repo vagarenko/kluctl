ef8488c27e86b04236ead9b555bec896daa786a0
Alexander Block <ablock84@gmail.com>
2023-03-20 20:24:43 +0100

    refactor: Add ObfuscateResult to Obfuscator

--- cmd/kluctl/commands/command_result.go
+++ cmd/kluctl/commands/command_result.go
@@ -207,23 +207,9 @@ func outputCommandResult(ctx context.Context, output []string, noObfuscate bool,
 
 	if !noObfuscate {
 		var obfuscator diff.Obfuscator
-		for _, c := range cr.ChangedObjects {
-			err := obfuscator.ObfuscateChanges(c.Ref, c.Changes)
-			if err != nil {
-				return err
-			}
-		}
-		for _, n := range cr.NewObjects {
-			err := obfuscator.ObfuscateObject(n.Object)
-			if err != nil {
-				return err
-			}
-		}
-		for _, h := range cr.HookObjects {
-			err := obfuscator.ObfuscateObject(h.Object)
-			if err != nil {
-				return err
-			}
+		err := obfuscator.ObfuscateResult(cr)
+		if err != nil {
+			return err
 		}
 	}
 
