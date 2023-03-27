da6927652f42d8d5391fe9e683c816a77be6f598
Alexander Block <ablock84@gmail.com>
2023-03-27 09:01:25 +0200

    fix: Fix GitUrl unmarshalling in typescript

--- pkg/webui/generate-ts/main.go
+++ pkg/webui/generate-ts/main.go
@@ -1,13 +1,15 @@
 package main
 
 import (
+	git_url "github.com/kluctl/kluctl/v2/pkg/git/git-url"
 	"github.com/kluctl/kluctl/v2/pkg/types/result"
 	"github.com/tkrajina/typescriptify-golang-structs/typescriptify"
 )
 
 func main() {
 	converter := typescriptify.New().
-		Add(result.CommandResult{})
+		Add(result.CommandResult{}).
+		ManageType(git_url.GitUrl{}, typescriptify.TypeOptions{TSType: "string", TSTransform: "source"})
 
 	err := converter.ConvertToFile("ui/src/models.ts")
 	if err != nil {
